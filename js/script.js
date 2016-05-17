27be11abc3fbdabebc4560aa919fad749a7b5bdb
Yury <phan@i.ua>
2016-05-17 12:46:53 +0300

    add pictures

--- js/script.js
+++ js/script.js
@@ -1,30 +1,33 @@
 function SearchCallback() {
 
   var data = arguments[0];
-  console.log(data);
   var html = $('#tmpl').html();
-  var content = tmpl(html, data);
+  var searchField = $('#searchField');
+  data.query = searchField.val();
 
+  var content = tmpl(html, data);
+  console.log(data);
   $('.searchResult').html('');
   $('.searchResult').append(content);
 
+  searchField.val('');
 }
 $(function() {
 
 
   function Search(query) {
     $.ajax({
-      url: 'https://pixabay.com/api/?key=2578289-585e40fcdc72ec40162cd6356&q=' + query + '&image_type=photo&per_page=10&callback=SearchCallback&context=?',
+      url: 'https://pixabay.com/api/?key=2578289-585e40fcdc72ec40162cd6356&q=' + query + '&image_type=photo&per_page=5&callback=SearchCallback&context=?',
       dataType: 'jsonp'
     });
   }
 
-  $('.searchButtom').click( function() {
+  $('.searchButtom').click(function() {
     var query = $('#searchField').val();
     Search(query);
   });
 
-  $('body').keydown( function(e){
+  $('body').keydown(function(e) {
     if (e.keyCode == 13) {
       var query = $('#searchField').val();
       e.preventDefault();
